78981f871fbf7ceb88c4fb2b7d35149f07667db0
furebo <furebodidace582@gmail.com>
2020-10-29 12:48:21 +0200

    loginuser changed

--- loginuser.js
+++ loginuser.js
@@ -5,7 +5,7 @@ const jsonwebtoken = require('jsonwebtoken');
 const router = express.Router();
 var usermodel = require('./usermodel');
 
-router.post('/loginuser',protection,(req,res,next)=>{
+router.post('/loginuser',(req,res,next)=>{
     usermodel.find({name:req.body.name})
     .then(user =>{ bcrypt.compare(req.body.password, user[0].password, ()=>{
         var token = jsonwebtoken.sign({name:user[0].name},"secret", {expiresIn:"10h"})
