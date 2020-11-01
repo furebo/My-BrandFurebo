80749598e783c417ab6b4457b9f149ee5d408a1a
furebo <furebodidace582@gmail.com>
2020-11-01 12:35:52 +0200

    changes deleteArticle file

--- deleteArticle.js
+++ deleteArticle.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json()) ;
  *         description: Successfully deleted
  */
 
-router.delete('/article/:id',protection,(req,res)=>{
+router.delete('/article/:id',(req,res)=>{
     articleModel.findByIdAndRemove({_id:req.params.id}).then(()=>{
         res.json({
             message:"Article deleted !",
