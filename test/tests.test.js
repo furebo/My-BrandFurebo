67a6daa6132839dd00820f2dbc854d3e725f5a58
furebo <furebodidace582@gmail.com>
2020-11-02 17:26:31 +0200

    modifies tests.test.js file

--- test/tests.test.js
+++ test/tests.test.js
@@ -15,6 +15,36 @@ const { should } = require('chai');
 chai.should();
 chai.use(chaiHttp);
 
+
+describe('/delete/articleId', () => {
+	it('it should delete an article by id', (done) => {
+       let auth = " ";
+        const valid_input = {
+            "name": "furebo",
+            "password": "fode123"
+        }
+        chai.request(server)
+              .post('/loginuser')
+              .send(valid_input)
+              .then((login_response)=>{
+                auth = 'Bearer '+ login_response.body.token;
+                let article = new articleModel({title: "node", description:"article descr",content: "about me", articleImage: "myimage"})
+                article.save((err, article) => {
+                    chai.request(server)
+                    .delete('/article/' + article._id)
+                    .set('authorization', auth)
+                    .end((err, res) => {
+                      res.should.have.status(200);
+                      res.body.should.have.property('message').eql("Article deleted !");
+      
+                      done();
+                    });
+            })
+
+		});
+	});
+});
+
      //testing GET/article route for getting a particular article
             
      describe("get/article/:id",()=>{
