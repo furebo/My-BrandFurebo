d415893cfd84d8f46742ca73d83935f9135ae689
furebo <furebodidace582@gmail.com>
2020-11-03 06:39:22 +0200

    moves delete article test to deletecomment.test.js file

--- test/deletecomment.test.js
+++ test/deletecomment.test.js
@@ -45,3 +45,32 @@ chai.use(chaiHttp);
 
         }) 
     })
+
+    describe('/delete/articleId', () => {
+        it('it should delete an article by id', (done) => {
+           let auth = " ";
+            const valid_input = {
+                "name": "furebo",
+                "password": "fode123"
+            }
+            chai.request(server)
+                  .post('/loginuser')
+                  .send(valid_input)
+                  .then((login_response)=>{
+                    auth = 'Bearer '+ login_response.body.token;
+                    let article = new articleModel({title: "node", description:"article descr",content: "about me", articleImage: "myimage"})
+                    article.save((err, article) => {
+                        chai.request(server)
+                        .delete('/article/' + article._id)
+                        .set('authorization', auth)
+                        .end((err, res) => {
+                          res.should.have.status(200);
+                          res.body.should.have.property('message').eql("Article deleted !");
+          
+                          done();
+                        });
+                })
+    
+            });
+        });
+    });
